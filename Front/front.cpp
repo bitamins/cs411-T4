26362df6161a5019e70600b782d40200b59d7521
downtolearn <msbridges99@gmail.com>
2018-02-09 01:17:03 -0800

    refactored code; initialized the database connection at the beggining of the program; passed database connection through program by reference to save memory and time.

--- Front/front.cpp
+++ Front/front.cpp
@@ -2,102 +2,90 @@
 Purpose: simple command line interface for connecting to
           the news database.
 */
+//libraries
 #include <iostream>
 #include <time.h>
 #include <pthread.h>
 #include <mysql++.h>
-// To compile you must link the libraries, included in makefile
-//  g++ front.cpp -o test -I/usr/include/mysql++ -I/usr/include/mysql -lmysqlpp
+
+//Global variables
+const char* serverIP = "54.241.100.64";
+const char* databaseName = "aggregator";
+
+//prototypes
+void interface(mysqlpp::Connection&);
+int fetch_news(mysqlpp::Connection&);
+mysqlpp::Connection db_connect(const char*,const char*,const char*,const char*);
+
+//default namespace
 using namespace std;
 
-void interface(char*,char*);
-int fetch_news(mysqlpp::Connection,clock_t,const char*,const char*);
-void print_news();
-
+/*
+	Function definitions
+*/
 int main(int argc, char* argv[]){
-
-  interface(argv[1],argv[2]);
+	//establish connection to database
+	mysqlpp::Connection db_conn = db_connect(argv[1],argv[2],serverIP,databaseName);
+	if(db_conn == NULL){
+		cout << "Exiting...\n"; return 0;
+	}
+	//launch the user interface with the database connection
+  interface(db_conn);
 }
 
-void interface(char* user, char* pass){
-
-	mysqlpp::Connection conn(false);
-	clock_t timer;
-
+void interface(mysqlpp::Connection& conn){
   int command=0;
   // run until stopped
+	cout << "Welcome to Real News, press (2) to exit the program.\n";
   while(true){
     cout << "Command: ";
     cin >> command;
     // switch on command
     switch(command){
-      case 0: cout << "hello\n"; break;
-			case 1: timer=clock(); fetch_news(conn,timer,user,pass); break;
-			case 2: cout << "goodbye\n"; break;
-			default: command = 1;
+      case 0: cout << "Hello\n"; break;
+			case 1: cout << "Fetching News\n";fetch_news(conn); break;
+			case 2: cout << "Goodbye\n"; break;
+			default: command = 2;
     }
     //exit case
-    if(command == 1){
+    if(command == 2){
       cout << "end.\n";
       break;
     }
   }
 }
 
-int fetch_news(mysqlpp::Connection conn, clock_t timer,const char* user_in,const char* pass_in){
-  cout << "Fetching News\n";
+mysqlpp::Connection db_connect(const char* user,const char* pass,const char* server,const char* dbase){
+	mysqlpp::Connection conn(false);
+	if(conn.connect(dbase,server,user,pass)){
+		return conn;
+	}
+	else{
+		cerr << "DB connection failed: " << conn.error() << endl;
+	}
+}
 
-	//database information
-	const char* user = user_in;
-	const char* pass = pass_in;
-	const char* server = "54.241.100.64";
-	const char* database = "aggregator";
-
-
-	//connect to database
-	cout << "Initialize connection: ";
-	  cout << "Fetching News\n";
-	// timer = clock();
-
-	// timer = clock() - timer;
-	// cout << ((float)timer/CLOCKS_PER_SEC) << endl;
-
-	cout << "Establish connection: ";
-		  cout << "Fetching News\n";
-
-	if(conn.connect(database,server,user,pass)){
-			  cout << "Fetching News\n";
-
-
-		//retrieve database table column
+int fetch_news(mysqlpp::Connection& conn){
+		//query the database
 		mysqlpp::Query query = conn.query("SELECT * FROM Business;");
-		cout << "Create query" << endl;
-		if(mysqlpp::StoreQueryResult res = query.store()){
-			cout << "Store query" << endl;
+		//store results
+		if(mysqlpp::StoreQueryResult results = query.store()){
 			cout << "\nNews:\n";
+			//create query iterator
 			mysqlpp::StoreQueryResult::const_iterator it;
-			for(it=res.begin(); it!=res.end(); ++it){
+			//iterate through the rows
+			for(it=results.begin(); it!=results.end(); ++it){
 				mysqlpp::Row row = *it;
+				//print the columns of each row
 				for(int i=0;i<6;i++){
 					cout << '\t' << row[i];
 				}
 				cout << endl;
 			}
 			cout << endl;
-			timer = clock() - timer;
-			cout << ((float)timer/CLOCKS_PER_SEC) << endl;
 		}
 		else{
 			cerr << "Failed to retrieve news:" << query.error() << endl;
 			return 1;
 		}
-	}
-	else{
-		cerr << "DB connection failed: " << conn.error() << endl;
-		return 1;
-	}
-}
-
-void print_news(){
-  cout << "printing\n";
 }
