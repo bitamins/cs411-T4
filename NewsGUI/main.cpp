6d2392b27e462affe9799dcf8e16b0c456e782e7
tgriff073 <griff073@cougars.csusm.edu>
2018-02-27 18:46:09 -0800

    Added error checking for command arguments

--- NewsGUI/main.cpp
+++ NewsGUI/main.cpp
@@ -3,7 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
+    if(argc != 3)
+    {
+           qDebug() << "Username and Password not stored as commandline arguments";
+           a.exit();
+           return 0;
+    }
     MainWindow w(QString::fromStdString(argv[1]),QString::fromStdString(argv[2]));
     w.show();
 
